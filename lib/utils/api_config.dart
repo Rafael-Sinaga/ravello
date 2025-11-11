1905d32b11dd545860978f3bafbe8fcdd1eb1c20
WafiAkbar12 <wafiakbarr@gmail.com>
2025-11-11 13:59:52 +0700

    abe;

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,6 +1,6 @@
 // lib/utils/api_config.dart
 class ApiConfig {
   // Gunakan IP backend teman kamu (pastikan satu jaringan)
-  static const String baseUrl = "http://10.38.48.37:3000";
+  static const String baseUrl = "http://10.38.54.231:3000";
 }
 
